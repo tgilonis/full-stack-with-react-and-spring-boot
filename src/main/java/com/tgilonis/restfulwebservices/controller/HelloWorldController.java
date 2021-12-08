07a3cc530c2177e67634a97620488f9b71e37114
tgilonis <tgilonis@protonmail.com>
2021-12-08 21:44:58 +0000

    Refactored to GetMapping

--- src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.tgilonis.restfulwebservices.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ public class HelloWorldController
     // GET
     // URI - /hello-world
     // method - "Hello World"
-    @RequestMapping(method= RequestMethod.GET, path="/hello-world")
+    @GetMapping(path="/hello-world")
     public String helloWorld()
     {
         return "Hello World!";
