182858c8784d698306f689d2e94bdbeaa47ae188
tgilonis <tgilonis@protonmail.com>
2021-12-08 22:15:01 +0000

    Added request using path variable

--- src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.tgilonis.restfulwebservices.controller;
 
 import com.tgilonis.restfulwebservices.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller - handles HTTP (rest) requests
@@ -17,12 +18,17 @@ public class HelloWorldController
         return "Hello World!";
     }
 
-
-
     // hello-world-bean
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean()
     {
         return new HelloWorldBean("Hello World!");
+    }
+
+    // hello-world-bean/path-variable/{name}
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name)
+    {
+        return new HelloWorldBean(String.format("Hello World, %s!", name));
     }
 }
