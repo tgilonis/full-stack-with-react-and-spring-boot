320839c905b564e06e80cdeaf519b6af732440c5
tgilonis <tgilonis@protonmail.com>
2021-12-08 21:44:15 +0000

    Imported spring project, implemented simple hello world example

---
+++ src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.tgilonis.restfulwebservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller - handles HTTP (rest) requests
+@RestController
+public class HelloWorldController
+{
+    // GET
+    // URI - /hello-world
+    // method - "Hello World"
+    @RequestMapping(method= RequestMethod.GET, path="/hello-world")
+    public String helloWorld()
+    {
+        return "Hello World!";
+    }
+}
