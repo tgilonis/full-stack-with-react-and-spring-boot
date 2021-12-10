9afe0588f59dd6d3d44f00032753c488ac522697
tgilonis <tgilonis@protonmail.com>
2021-12-10 18:15:00 +0000

    Allows CORS from localhost 4200

--- src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/tgilonis/restfulwebservices/controller/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.tgilonis.restfulwebservices.controller;
 
 import com.tgilonis.restfulwebservices.HelloWorldBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller - handles HTTP (rest) requests
 @RestController
+// Allow requests from localhost 4200
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController
 {
     // GET
