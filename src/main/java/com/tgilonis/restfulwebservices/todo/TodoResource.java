0b86955e1f81011eddfca0030939aa4f61c1332b
tgilonis <tgilonis@protonmail.com>
2021-12-11 11:11:55 +0000

    Enabled cors

--- src/main/java/com/tgilonis/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/tgilonis/restfulwebservices/todo/TodoResource.java
@@ -2,6 +2,7 @@ package com.tgilonis.restfulwebservices.todo;
 
 import com.tgilonis.restfulwebservices.todo.services.TodoHardCodedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoResource
 {
     @Autowired
