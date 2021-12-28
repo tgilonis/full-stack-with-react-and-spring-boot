bc531a2cbc064f4f730ee1e4d16c29e57734a023
tgilonis <tgilonis@protonmail.com>
2021-12-28 15:42:19 +0000

    Added authentication bean api

---
+++ src/main/java/com/tgilonis/restfulwebservices/basic/controller/BasicAuthenticationController.java
@@ -0,0 +1,23 @@
+package com.tgilonis.restfulwebservices.basic.controller;
+
+import com.tgilonis.restfulwebservices.basic.AuthenticationBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller - handles HTTP (rest) requests
+@RestController
+// Allow requests from localhost 4200
+@CrossOrigin(origins = "http://localhost:4200")
+public class BasicAuthenticationController
+{
+
+    @GetMapping(path="/basicauth")
+    public AuthenticationBean helloWorldBean()
+    {
+        return new AuthenticationBean("You are Authenticated!");
+    }
+
+
+
+}
