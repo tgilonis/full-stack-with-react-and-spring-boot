89d45a16f238c6466c5fc1cd2ef6cebd268c02ff
tgilonis <tgilonis@protonmail.com>
2022-01-08 17:23:37 +0000

    Added new user to in memory authentication

---
+++ src/main/java/com/tgilonis/rest/webservices/restfulwebservices/BcryptEncoderTest.java
@@ -0,0 +1,19 @@
+package com.tgilonis.rest.webservices.restfulwebservices;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptEncoderTest
+{
+    public static void main(String[] args)
+    {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(encoder.encode("password@!23@#!"));
+
+        }
+    }
+
+
+}
+
